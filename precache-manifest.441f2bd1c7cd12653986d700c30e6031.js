498f39d8b1fbbec7cd21b15e50a44e19bf49f341
Carl Holmquist <cwv.holmquist@gmail.com>
2020-07-30 11:56:55 -0500

    Updates

---
+++ precache-manifest.441f2bd1c7cd12653986d700c30e6031.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9496292fba026dbc501dd35763382299",
+    "url": "/basics_react_course/index.html"
+  },
+  {
+    "revision": "eb53a7ac893b8fad368b",
+    "url": "/basics_react_course/static/css/main.a25fad57.chunk.css"
+  },
+  {
+    "revision": "eb32b41f166e99422f98",
+    "url": "/basics_react_course/static/js/2.ba95ac2b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/basics_react_course/static/js/2.ba95ac2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb53a7ac893b8fad368b",
+    "url": "/basics_react_course/static/js/main.a7bd6002.chunk.js"
+  },
+  {
+    "revision": "54e5e3e8710ef5ccc330",
+    "url": "/basics_react_course/static/js/runtime-main.818a27c2.js"
+  }
+]);
